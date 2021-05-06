e7f11131cdd610ee6fdbc9caf2000bb0d4ee301b
Mamoru <49065044+AlexMosiakin@users.noreply.github.com>
2021-05-06 17:48:35 +0300

    Movie detail

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import home1 from '../img/home1.png';
 
+//Framer Motion
+import {motion} from 'framer-motion';
+
 //Styled
 import styled from 'styled-components';
 import{About,Description,Image,Hide} from '../styles';
@@ -11,7 +14,7 @@ const AboutSection = () => {
             <Description>
                 <div className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 animate={{}}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
                         <h2>your <span>dreams</span> come </h2>
